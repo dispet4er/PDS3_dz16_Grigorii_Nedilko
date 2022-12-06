f69a95fbb644a8254d1942b7bb30e7b1e6a78792
Grigorii Nedilko <nedilko.gp@gmail.com>
2022-12-06 11:30:11 +0200

    addin sql file with dz_16

---
+++ dz_16.sql
@@ -0,0 +1,20 @@
+    #all_from_employees_sort_name_a-z
+SELECT *
+FROM pds.employees
+ORDER BY FIRST_NAME ASC;
+
+    #name_salary_+_tax_as_15%_from_salary
+SELECT FIRST_NAME, LAST_NAME, SALARY, SALARY * .15 as TAX
+FROM pds.employees;
+
+    #sum_salary_all
+SELECT sum(SALARY) as SumSalary
+FROM employees;
+
+    #max_min_salary_all
+SELECT max(SALARY) as MaxSalary, min(SALARY) as MinSalary
+FROM employees;
+
+    #avg_salary_+_num_of_employees
+SELECT count(EMPLOYEE_ID) as NumberOfEmployees, avg(SALARY) as AvgSalary
+FROM employees;
